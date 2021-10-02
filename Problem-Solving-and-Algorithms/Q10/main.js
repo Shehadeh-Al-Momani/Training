6a9e6c47ad6401953333bb302c47f57c94fae9de
Shehadeh Al-Momani <shehadeh.almomani@gmail.com>
2021-10-02 11:59:39 +0300

    first commit

---
+++ Problem-Solving-and-Algorithms/Q10/main.js
@@ -0,0 +1,19 @@
+console.log('Problem Solving Q: 10');
+
+/* Times Of Most Freq Char */
+
+/*  
+Write a function that returns the number of occurrences of the most frequent character in a sentence.
+*/
+
+function timesOfMostFreqChar(str) {
+
+}
+
+/*
+Examples:
+timesOfMostFreqChar('hello world') // => 3
+timesOfMostFreqChar('hello world lol') // => 5
+timesOfMostFreqChar('hello world lol woooow') // => 7
+timesOfMostFreqChar('hello world lol wow www.com') // => 6
+*/
